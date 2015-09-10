492c5258a86c14fbc43b8080b8df35c2e12f511a
Bill Grenard <grenardw@berkeley.edu>
2015-09-09 17:34:24 -0700

    REF: Improve readability. Separate business logic from presentation logic.

--- showtimes_finder.py
+++ showtimes_finder.py
@@ -21,7 +21,6 @@ def present_instructions():
 def process_input():
     """Return HTML from proper page determined by the user's input."""
 
-    # FIXME: Use exception handling here.
     while True:
         location = raw_input("\n\nPlease enter your location in the following"
                              "format: City St. e.g. Berkeley CA: ")
@@ -39,7 +38,6 @@ def process_input():
                    "and a state.")
 
     # Gather date info and account for errors.
-    # FIXME: Use exception handling here.
     while True:
         date = raw_input("\n\nPlease enter the date you wish to see the movie"
                          "in the following format mm/dd/yyyy: ")
@@ -70,120 +68,132 @@ def process_input():
     return urllib2.urlopen(url)  # Return info from the URL.
 
 
-def pull_theater_titles(html):
-    """Return a list of available theater titles from the html in theaters list."""
-
-    title_list = []  # List to hold theater titles.
-    # Split the html into a list whose elements each have info on only one theater.
-    theaters = html.split('showtimes-theater-title')[1:]
-
-    # FIXME: Use a list comprehension or enumeration here. Avoid global vars use.
-    for index in range(len(theaters)):
-        # If 'there are no movies' is not found, there are showtimes to be displayed
-        if theaters[index].find('there are no movies') == -1:
-            terminating_index = theaters[index].find('</a>')
-            temp_list = theaters[index][:terminating_index].split()
-            title_str = ' '.join(temp_list[2:])
-            title_list.append(title_str)
-
-    return (theaters, title_list)
-
-
-def pull_movie_titles(theaters):
-    """Return a list of movie titles for each theater.
-
-    The list returned is a list of lists. The inner lists are filled with the
-    movie titles. Each list element represents movies at a different theater.
-    """
-
-    inner_list = []  # List to temporarily hold movie titles from one single theater
-    title_list = []  # List to hold inner lists.
-    movies = []
-
-    # Loop through the theater strings and split each string by movie.
-    for theater in theaters:
-        movies.append(theater.split('showtimes-movie-container')[1:])
-
-    # Here the outer loop goes through the movies list, and inner loop goes
-    # through the strings of each element in the movies list.
-    # FIXME: Use list comprehension or enumeration.
-    for i in range(len(movies)):
-        for j in range(len(movies[i])):
-            # If there are movies to display, then add them to the list
-            if movies[i][j].find('there are no movies') == -1:
-                # The title starts 5 chars after 'alt'.
-                starting_index = movies[i][j].find('alt') + 5
-                # The title ends just before 'showtimes and tickets'
-                terminating_index = movies[i][j].find('showtimes and tickets')
-                # Remove whitespace
-                temp_list = movies[i][j][starting_index:terminating_index].split()
-                title_str = ' '.join(temp_list)
-                inner_list.append(title_str)
-
-        if len(inner_list) > 0:
-            title_list.append(inner_list)
-
-        inner_list = []
-
-    return (movies, title_list)
-
-
-def determine_showtimes(search_str):
-    """Return a list of showtimes from search_str.
+def split_html(html):
+    """Return a tuple containing three lists, with theater, movie, and showtime info.
 
     Args:
-        search_str (str) -- The string to be searched for showtimes.
+        html (str) -- The text html code containing the showitmes information.
 
-    Note that for this function to work it must be passed a string from an
-    element of the movies[] list.
+    The first element of the tuple is a list of strings, each containing
+    information for a different theater. The second element is a list of lists.
+    Each inner list corresponds to a theater, whose list elements are strings
+    containing information about each movie playing at that theater. Finally,
+    the third element is a 3 dimensional list, at its deepest level containing
+    information for each showtime for a given movie at a given theater.
+    """
+
+    # Split the html by theater.
+    theater_info = html.split('showtimes-theater-title')[1:]
+
+    # Split each theather text by movie.
+    movie_info = [theater.split('showtimes-movie-container')[1:] for theater in theater_info]
+
+    # Split each movie for a given theater by showtimes.
+    showtimes_info = []
+
+    for movie_list in movie_info:
+        temp_list = [movie.split('</time>')[:-1] for movie in movie_list]
+        showtimes_info.append(temp_list)
+
+    """
+    showtimes_info = [movie.split('</time>')[:-1]
+                      for movie_list in movie_info
+                      for movie in movie_list]
+    """
+    return (theater_info, movie_info, showtimes_info)
+
+
+def pull_theater_titles(theater_info):
+    """Return a list of available theater titles from the theater info list.
+
+    Args:
+        theater_info (list) -- Each element is a string with information about
+                               a different theater.
+    """
+
+    # Split each theater string between '</a>' and '>' where the time is located
+    # if there are movies playing.
+    title_list = [theater.split('</a>', 1)[0].split('>', 1)[1].strip()
+                  for theater in theater_info
+                  if 'there are no movies' not in theater]
+
+    return title_list
+
+
+def pull_movie_titles(movie_info):
+    """Return a list containing lists of movie titles for each theater.
+
+    Args:
+        movie_info (list) -- Each element is a list which contains information
+                             on all movies for a given theater.
+
+    The list returned is a list of lists. The inner lists are filled with the
+    movie titles. Each inner list contains the titles of movies for a different
+    theater.
+    """
+
+    title_list = []
+
+    # Each movie_list holds the movie info for a different theater.
+    for movie_list in movie_info:
+        # Split each move_list to get movie titles, if there are movies playing.
+        inner_list = [movie.split('showtimes and tickets', 1)[0].split('alt="', 1)[1].strip()
+                      for movie in movie_list
+                      if 'there are no movies' not in movie]
+        title_list.append(inner_list)
+
+    return title_list
+
+
+def pull_showtimes(showtimes_info):
+    """Return a 3 dimensional list containing showtimes by movie and theater.
+
+    Args:
+        showtimes_info (list) -- A 3 dimensional list of information on showtimes
+                                 by movie and theater.
+
+    The list returned is a 3 dimensional list. The innermost list contains
+    showtimes, for a given movie at a given theater.
     """
 
     showtimes_list = []
-    search_index = 0    # Marks the starting index of the search.
+    theater_list = []
 
-    while True:
-        terminating_index = search_str.find('</time>', search_index)
-
-        # Break if all times have been found.
-        if terminating_index == -1:
-            break
-
-        # If this condition is met then this is the index where the time starts.
-        if search_str[terminating_index - 7] != '>':
-            showtimes_list.append(search_str[terminating_index - 7: terminating_index])
-        # Otherwise the time begins 6 chars before terminating index
-        else:
-            showtimes_list.append(search_str[terminating_index - 6: terminating_index])
-
-        search_index = terminating_index + 1
+    for movie_list in showtimes_info:
+        for movie in movie_list:
+            times = [showtime[-6:] if showtime[-7] == '>'
+                     else showtime[-7:]
+                     for showtime in movie]
+            theater_list.append(times)
+        showtimes_list.append(theater_list)
 
     return showtimes_list
 
 
-def present_showtimes(theater_titles, movie_titles, movies):
-    """Present the theaters, movies, and showtimes in a formatted manner."""
+def present_showtimes(theater_titles, movie_titles, showtimes):
+    """Present the theaters, movies, and showtimes, in a formatted manner.
 
-    # FIXME: Use list comprehensions or enumerations.
-    for i in range(len(theater_titles)):
-        print '\nTheater: ' + theater_titles[i] + '\n'
+    Args:
+        theater_titles (list) -- A list of theater titles.
+        movie_titles (list) -- A 2 dimensional list of movie titles separated by theater.
+        showtimes (list) -- A 3 dimensional list of showtimes separated by movie
+                            and theater
+    """
 
-        for j in range(len(movie_titles[i])):
-            print '  Movie: ' + movie_titles[i][j]
+    for (theater, theater_title) in enumerate(theater_titles):
+        print '\nTheater: ' + theater_title + '\n'
 
-            showtimes = determine_showtimes(movies[i][j])
-            for showtime in showtimes:
+        for (movie, movie_title) in enumerate(movie_titles[theater]):
+            print '  Movie: ' + movie_title
+
+            for showtime in showtimes[theater][movie]:
                 print '         ' + showtime
             print ''
 
 
-def is_url_valid(html):
-    """Return true if url was found. Return false if not."""
-    # FIXME: Does this really need to be a function?
-    return html.find("This location can't be found.") == -1
-
-
 def run_showtimes_finder():
     """Run the showtimes_finder program."""
+
     present_instructions()
     response = ''
     html = ''
@@ -192,23 +202,25 @@ def run_showtimes_finder():
         response = process_input()
         html = response.read()  # Read information from the URL
 
-        if is_url_valid(html):
+        # If the html is valid, then program execution can continue.
+        if html.find("This location can't be found.") == -1:
             break
         else:
             print("\nThe location you entered cannot be found. Please check that "
                   "your location was spelled correctly and entered in the correct "
                   "format")
 
-    theaters = pull_theater_titles(html)[0]
-    theater_titles = pull_theater_titles(html)[1]
-    movies = pull_movie_titles(theaters)[0]
-    movie_titles = pull_movie_titles(theaters)[1]
+    # Split the html by where theater, movie, and showtimes info is located.
+    theater_info, movie_info, showtimes_info = split_html(html)
 
+    # Pull the titles and showtimes from the respective chunks of html text.
+    theater_titles = pull_theater_titles(theater_info)
+    movie_titles = pull_movie_titles(movie_info)
+    showtimes = pull_showtimes(showtimes_info)
 
-    present_showtimes(theater_titles, movie_titles, movies)
+    present_showtimes(theater_titles, movie_titles, showtimes)
     response.close()        #close the urllib2 connection file
 
 
 if __name__ == "__main__":
     run_showtimes_finder()
-
